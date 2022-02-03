a38881be058a5ace5c8083d97bd34c3e7484afb1
Anton Yefimov <anton.yefimov19@gmail.com>
2022-02-03 17:42:54 +0200

    created logic for adding games in cart

--- src/features/shop/components/GameCard.jsx
+++ src/features/shop/components/GameCard.jsx
@@ -2,26 +2,37 @@ import React from "react"
 import "../../../assets/css/shop/gameCard/gameCard.css"
 import { ImageCover } from "./ImageCover"
 import { GameTag } from "./GameTag"
+import { useDispatch } from "react-redux"
+import { setItemInCart } from "../../../redux/shop/reducers/cartReducer"
 
-export const GameCard = ({ name, tags, price, imageUrl }) => {
+export const GameCard = ({ game }) => {
+    const dispatch = useDispatch()
 
     const renderGameTags = () => {
-        return tags.map((tag, index) => (
+        return game.tags.map((tag, index) => (
             <GameTag key={index} tag={tag}/>
         ))
     }
 
+    const addGameToCartHandler = (event) => {
+        event.stopPropagation()
+        dispatch(setItemInCart(game))
+    }
+
     return (
         <div className="game-card">
-            <ImageCover imageUrl={ imageUrl }/>
+            <ImageCover imageUrl={ game.imageUrl }/>
             <div className="game-card__details">
-                <h2 className="game-card__name">{ name }</h2>
+                <h2 className="game-card__name">{ game.name }</h2>
                 <div className="game-card__tags">
-                    { tags && tags.length ? renderGameTags() : "" }
+                    { game.tags && game.tags.length ? renderGameTags() : "" }
                 </div>
                 <div className="game-card__buy-wrapper">
-                    <p className="game-card__price">{ price }</p>
-                    <button className="game-card__button">В корзину</button>
+                    <p className="game-card__price">{ game.price }</p>
+                    <button
+                        className="game-card__button"
+                        onClick={addGameToCartHandler}
+                    >В корзину</button>
                 </div>
             </div>
         </div>
