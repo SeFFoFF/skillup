4e148bd53f15250f547021c1a527064281560d83
Anton Yefimov <anton.yefimov19@gmail.com>
2022-02-15 11:40:19 +0200

    Created loader and fixed dark theme style (#11)

---
+++ src/features/shop/components/Loader.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+import "../../../assets/css/shop/loader.css"
+
+export const Loader = () => {
+    return (
+        <div className="loader">
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+            <div/>
+        </div>
+    )
+}
